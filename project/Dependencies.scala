2393a3db64caa1a44254d2eeadf05981df22ac2d
Edwin Wood <woode@dnb.com>
2018-07-11 07:29:28 -0500

    basic server

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,9 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  val dependencies = Seq(
+    "com.typesafe.akka" %% "akka-http"   % "10.1.3",
+    "com.typesafe.akka" %% "akka-stream" % "2.5.12",
+    "org.scalatest" %% "scalatest" % "3.0.5" % Test
+  )
 }
--- src/main/scala/net/treywood/http/Main.scala
+++ src/main/scala/net/treywood/http/Main.scala
@@ -1,9 +1,35 @@
 package net.treywood.http
 
-class Main extends App {
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+object Main extends App {
 
   override def main(args: Array[String]): Unit = {
-    println("Hello")
+
+    implicit val system: ActorSystem = ActorSystem("http-server")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+    implicit val dispatcher: ExecutionContextExecutor = system.dispatcher
+
+    val routes = get {
+      path("hello") {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Hello World</h1>"))
+      }
+    }
+
+    val binding = Http().bindAndHandle(routes, "localhost", 8080)
+
+    println("Server running")
+    StdIn.readLine()
+
+    binding.flatMap(_.unbind()).onComplete(_ => system.terminate())
   }
 
 }
